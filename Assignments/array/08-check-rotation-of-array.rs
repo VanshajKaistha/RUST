ba5c00adddccfeb98a838544c20f831864b78344
Lonewolfvk <kvanshaj@gmail.com>
2025-05-11 16:06:54 +0530

    to do q8 to check rotation of array is there or not

--- Assignments/array/08-check-rotation-of-array.rs
+++ Assignments/array/08-check-rotation-of-array.rs
@@ -1,5 +1,15 @@
 fn main()
 {
-    let arr:[i32;5] = [1,2,3,4,5];
-    let arr:[i32;5] = [1,2,3,4,5];
+    let arr1:[i32;5] = [1,2,3,4,5];
+    let arr2:[i32;5] = [4,5,1,2,3];
+    
+    if arr1.len() == arr2.len()
+    {
+        println!("not a rotation of array");
+    }
+    else
+    {
+        let // todo
+    }
+
 }
